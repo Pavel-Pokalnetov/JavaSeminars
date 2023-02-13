4097ed248328b6df7fd917b1813147eb73e63514
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-02-13 23:22:23 +0500

    семинар 2

---
+++ src/main/java/Seminar2/Main.java
@@ -0,0 +1,68 @@
+package src.main.java.Seminar2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+//        task1(10,'A','B');
+//        task1(11,'A','B');
+//        System.out.println( compressString("aaaasssddsdddaa"));
+//        System.out.println(palindrom("шалаш"));
+        writeMyFile();
+    }
+
+    public static void task1(int N, char c1, char c2) {
+//        if (N<=0) return;
+        StringBuilder result = new StringBuilder();
+        while (N > 0) {
+            result.append(c1);
+            N--;
+            if (N == 0) break;
+            result.append(c2);
+            N--;
+        }
+        System.out.println(result);
+    }
+
+    public static String compressString(String incomingString) {
+        StringBuilder tempString = new StringBuilder();
+        char charOld = incomingString.charAt(0);
+        tempString.append(charOld);
+        for (int i = 1; i < incomingString.length(); i++) {
+            if (charOld != incomingString.charAt(i)) {
+                tempString.append(incomingString.charAt(i));
+                charOld=incomingString.charAt(i);
+            }
+        }
+        return new String(tempString);
+    }
+
+    public static boolean palindrom(String incomingString){
+        return  (incomingString.equals(new StringBuilder(incomingString).reverse().toString()));
+    }
+
+
+    public static String get100TEST(){
+        StringBuilder content = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            content.append("TEST\n");
+        }
+        return content.toString();
+    }
+    public static void writeMyFile(){
+
+        try {
+            FileWriter writer = new FileWriter("TEST.txt");
+           try {
+               writer.write(get100TEST());
+           }catch (IOException e){
+               throw new RuntimeException(e);
+           } finally {
+               writer.close();
+           }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
