1b89df6ba8eda052edfa1d62bfeaae8355f3e5eb
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-02-14 00:11:30 +0500

    семинар 2 окончен

--- src/main/java/Seminar2/Main.java
+++ src/main/java/Seminar2/Main.java
@@ -2,6 +2,7 @@ package src.main.java.Seminar2;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,8 @@ public class Main {
 //        task1(11,'A','B');
 //        System.out.println( compressString("aaaasssddsdddaa"));
 //        System.out.println(palindrom("шалаш"));
-        writeMyFile();
+//        writeMyFile();
+        getMyListFiles();
     }
 
     public static void task1(int N, char c1, char c2) {
@@ -32,35 +34,58 @@ public class Main {
         for (int i = 1; i < incomingString.length(); i++) {
             if (charOld != incomingString.charAt(i)) {
                 tempString.append(incomingString.charAt(i));
-                charOld=incomingString.charAt(i);
+                charOld = incomingString.charAt(i);
             }
         }
         return new String(tempString);
     }
 
-    public static boolean palindrom(String incomingString){
-        return  (incomingString.equals(new StringBuilder(incomingString).reverse().toString()));
+    public static boolean palindrom(String incomingString) {
+        return (incomingString.equals(new StringBuilder(incomingString).reverse().toString()));
     }
 
 
-    public static String get100TEST(){
+    public static String get100TEST() {
         StringBuilder content = new StringBuilder();
         for (int i = 0; i < 100; i++) {
             content.append("TEST\n");
         }
         return content.toString();
     }
-    public static void writeMyFile(){
+
+    public static void writeMyFile() {
 
         try {
             FileWriter writer = new FileWriter("TEST.txt");
-           try {
-               writer.write(get100TEST());
-           }catch (IOException e){
-               throw new RuntimeException(e);
-           } finally {
-               writer.close();
-           }
+            try {
+                writer.write(get100TEST());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                writer.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void getMyListFiles() {
+        File dir = new File("C:\\MASTER\\Seminars");
+        StringBuilder listFiles = new StringBuilder();
+        if (dir.isDirectory()) {
+            for (File item : dir.listFiles()) {
+                listFiles.append(item.getName() + '\n');
+            }
+        }
+        try {
+            FileWriter writer = new FileWriter("C:\\MASTER\\Seminars\\filelist.txt");
+            try {
+                writer.write(listFiles.toString());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                writer.close();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
