1d000b64706a638f84a7ecbd0c371d14704590ee
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-02-27 13:28:03 +0500

    задание готово

--- src/main/java/HomeWork5/Main.java
+++ src/main/java/HomeWork5/Main.java
@@ -1,7 +1,10 @@
 package HomeWork5;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
 
 /*
 * Учитывая два целочисленных массива nums1 и nums2, верните максимальную длину подмассива, который появляется в обоих массивах.
@@ -19,13 +22,83 @@ Explanation: Повторяющийся подмассив с максималь
 * */
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 2, 1};//j
-        int[] nums2 = {3, 2, 1, 4, 7};//i
-        HashMap<Integer, ArrayList<Integer>> result = new HashMap<>();
-        int count = 0;
-        int index2 = 0;
-        while (index2< nums2.length){
-            
+        task1();
+        task2();
+    }
+
+    private static void task1() {
+        int[] nums1 = {1, 2, 3, 2, 1};
+        int[] nums2 = {3, 2, 1, 4, 7};
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+        findMaxSubArray(nums1, nums2);
+        System.out.println();
+    }
+
+    private static void task2() {
+        int[] nums1 = new int[]{0, 0, 0, 0, 0, 0};
+        int[] nums2 = new int[]{9, 0, 1, 0, 2, 1};
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+        findMaxSubArray(nums1, nums2);
+        System.out.println();
+    }
+//==========================================================================
+
+    /**
+     * @param nums1 - первый массивов
+     * @param nums2 - второй массивов
+     * @return -  подмассив максимального размера
+     */
+    private static void findMaxSubArray(int[] nums1, int[] nums2) {
+
+        HashSet<ArrayList<Integer>> tempSet1 = getAllSet(nums1);  // получаем множества
+        HashSet<ArrayList<Integer>> tempSet2 = getAllSet(nums2);  // подмассивов каждого входного массива
+        tempSet2.retainAll(tempSet1); // находим пересечение множеств
+        int max = 0;
+        for (ArrayList<Integer> item : tempSet2) { // находим самы большой размер подмассива
+            if (max < item.size()) {
+                max = item.size();
+            }
         }
+        ArrayList<ArrayList<Integer>> resultArrays = new ArrayList<>();
+        for (ArrayList<Integer> item : tempSet2) { // сохраняем найденные подмассивы с список
+            if (max == item.size()) {
+                resultArrays.add(item);
+            }
+        }
+        if (resultArrays.size()==0){
+            System.out.println("Нет повторяющихся подмассивов");
+            return;
+        }
+        if (resultArrays.size() > 1) {
+            System.out.println("Найдено несколько подмассивов максимального размера: ");
+        } else {
+            System.out.println("Подмассив максимального размера равен: ");
+        }
+        System.out.println(resultArrays);
+    }
+
+    /**
+     * Возвращает множество подмассивов исходного массива
+     *
+     * @param array - исходный массив
+     * @return - множество подмассивов исходного массива
+     */
+    private static HashSet<ArrayList<Integer>> getAllSet(int[] array) {
+        HashSet<ArrayList<Integer>> tempSet = new HashSet<>();
+        for (int n = 1; n <= array.length; n++) {
+            for (int i = 0; i < array.length; i++) {
+                ArrayList<Integer> tempArray = new ArrayList<>();
+                for (int j = i; j < i + n; j++) {
+                    if (j >= array.length) break;
+                    tempArray.add(array[j]);
+                }
+                if (tempArray.size() >= 2) {
+                    tempSet.add(tempArray);
+                }
+            }
+        }
+        return tempSet;
     }
 }
