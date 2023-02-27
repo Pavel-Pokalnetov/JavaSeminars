771f8f2c1ce22f799789f42cfc7c3264d15ba893
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-02-27 11:04:09 +0500

    stage 1

---
+++ src/main/java/HomeWork5/Main.java
@@ -0,0 +1,31 @@
+package HomeWork5;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+* Учитывая два целочисленных массива nums1 и nums2, верните максимальную длину подмассива, который появляется в обоих массивах.
+
+Example 1:
+Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+Output: 3
+Explanation: Повторяющийся подмассив с максимальной длиной равен [3,2,1].
+
+Example 2:
+Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+Output: 5
+Explanation: Повторяющийся подмассив с максимальной длиной равен [0,0,0,0,0].
+
+* */
+public class Main {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 2, 1};//j
+        int[] nums2 = {3, 2, 1, 4, 7};//i
+        HashMap<Integer, ArrayList<Integer>> result = new HashMap<>();
+        int count = 0;
+        int index2 = 0;
+        while (index2< nums2.length){
+            
+        }
+    }
+}
