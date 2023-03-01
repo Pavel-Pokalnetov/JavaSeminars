af8176294bdc8a10247a42ed88390e1f214771fb
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-03-01 11:47:41 +0500

    start

---
+++ src/main/java/HomeWork6/Main.java
@@ -0,0 +1,12 @@
+package HomeWork6;
+/*
+Учитывая {порядок следования} двоичного дерева, верните порядок обхода значений его узлов.
+пример
+    Input: root = [1,null,2,3]
+    Output: [1,2,3]
+    */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
