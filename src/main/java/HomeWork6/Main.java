993e55f6068b2a62ec9ce3feb6d6cde4010c63c8
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-03-01 18:02:08 +0500

    fix

--- src/main/java/HomeWork6/Main.java
+++ src/main/java/HomeWork6/Main.java
@@ -1,4 +1,8 @@
 package HomeWork6;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Учитывая {порядок следования} двоичного дерева, верните порядок обхода значений его узлов.
 пример
@@ -7,6 +11,34 @@ package HomeWork6;
     */
 public class Main {
     public static void main(String[] args) {
+        TreeNode tNode = new TreeNode(1,
+                new TreeNode(),
+                new TreeNode(2,
+                        new TreeNode(),
+                        new TreeNode(3)));
 
     }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            list.add(root.val);
+            list.addAll(preorderTraversal(root.left));
+            list.addAll(preorderTraversal(root.right));
+        }
+        return list;
+    }
+
+    public void fillTree(TreeNode root,Integer in){
+        if (root==null){
+            root.val=in;
+            return;
+        }else if (root.left==null){
+            fillTree(root.left,in);
+        }else{
+            fillTree(root.right,in);
+        }
+        return;
+    }
+
 }
